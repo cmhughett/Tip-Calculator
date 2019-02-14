be88d042051f55d2bf3971b91995c7a1e17a56b7
cmhug <cmhug@LAPTOP-AR07F35P>
2019-02-13 19:52:09 -0600

    Tip Calculator class week 4

--- Tip Calculator/Program.cs
+++ Tip Calculator/Program.cs
@@ -6,7 +6,56 @@ namespace Tip_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+            Console.WriteLine("What is your bill amount?");
+            string billInput = Console.ReadLine();
+            //billInput = billInput.Trim();
+            //billInput = billInput.Trim('$');
+            billInput = billInput.Trim().Trim('$');
+
+            double bill;
+            if (!double.TryParse(billInput, out bill))
+            {
+                Console.WriteLine($"The value {billInput} is not valid.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("What percent would you like to tip?");
+            string tipPercentInput = Console.ReadLine();
+            //tipPercentInput = tipPercentInput.Trim();
+            //tipPercentInput = tipPercentInput.Trim('%');
+            tipPercentInput = tipPercentInput.Trim().Trim('%');
+
+            double tipPercent;
+            if(!double.TryParse(tipPercentInput, out tipPercent))
+            {
+                Console.WriteLine($"The value {tipPercentInput} is not valid.");
+                Console.ReadLine();
+                return;
+            }
+
+           
+            //double bill = double.Parse(billInput);
+            //int tipPercent = int.Parse(tipPercentInput);
+
+            double tipAmount = tipPercent * bill / 100;
+            double totalBill = bill + tipAmount;
+            tipAmount = Math.Round(tipAmount, 2);
+            totalBill = Math.Round(totalBill, 2);
+
+            Console.WriteLine($"Your tip amount is ${tipAmount}.");
+            Console.WriteLine($"Your total bill is ${totalBill}.");
+            
+            Console.ReadLine();
+
+
+
+
+
+
         }
     }
 }
